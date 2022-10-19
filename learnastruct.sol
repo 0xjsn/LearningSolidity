df017cc1b8afa454d753b4fb02f1fe05a29e19fd
Jason <106914627+0xjsn@users.noreply.github.com>
2022-10-18 18:35:42 -0700

    Update learnastruct.sol
    fixed the main issues with the earlier code.

--- learnastruct.sol
+++ learnastruct.sol
@@ -1,32 +1,47 @@
 // SPDX-License-Identifier: MIT
 
 
-pragma solidity 0.8.8;
+pragma solidity ^0.8.8;
 
-contract learnastruct{
+contract learastruct{
     //so a struct is an object that can hold many data types
 
     //we create and initialize a struct that contains different animal types that we will define in a function
     struct Animals{
-        string FarmAnimal;
-        string Bird;
-        string Pet;
+        string text; //farmanimal
+        uint age; //age of the animal
+        string text2; //farm name 
     }
 
     //we define the struct Animals with the variable we want to map back to the struct (animals)
     //structName + variable name we want to store our strings (animal types) we created above
-    Animals animals; 
+    //initialize the state variable as animals list to be stored in an array
+    
+    Animals[] public animalslist;  
 
 
     //we create a function that maps the animals to the animal types in the struct, stored in the animals variable
-    function getAnimals() public {
-        animals = Animals('cow', 'parrot', 'dog');
-    }
     
-    //now with the animals stored, how do we create a function to return the animal types???
+    
+    //how do we create a function to store the animal
+
+    //we can do so using a function
+
+    function thenanimal(string memory _text, uint _age, string memory _text2) public { 
+        animalslist.push(Animals(_text, _age, _text2));
+    }
+
+    //we can fill the array with animals in different indices of the Animals array using the push method
+    //Then we can call the array (animals list) with the index number to get the details
+
+    
 
 
 }
+
+
+
+
 
 
 
